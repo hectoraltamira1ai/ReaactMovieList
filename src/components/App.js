fee9824be73d2977acd2bf93fffa4b8a9cae1bea
Hector <hectoraltamira1ai@gmail.com>
2024-11-04 22:10:14 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import MoviesList from './MoviesList';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MoviesList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
